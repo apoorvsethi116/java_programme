c484432f66326ef5f68f04e90be5d4715167e733
Apoorv Sethi <apoorv116oorv@gmail.com>
2025-04-10 20:00:52 +0530

    number is divisible by 5 and 11 or not

---
+++ src/divisible.java
@@ -0,0 +1,16 @@
+
+public class divisible {
+    public static void main(String[] args) {
+
+
+        int a = 99;
+        if (a % 5 == 0 && a % 11 == 0) {
+            System.out.println("Divisible");
+        } else {
+            System.out.println("Not divisible");
+        }
+    }
+
+
+
+}
